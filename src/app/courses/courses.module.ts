7852147e18f3c2cd4c97167f6c229dc9a867089a
Mohamed IDBRAHIM <idbrahimdev@gmail.com>
2023-08-03 21:28:25 +0100

    ngModel dataBinding

--- src/app/courses/courses.module.ts
+++ src/app/courses/courses.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ListCoursesComponent } from './list-courses/list-courses.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [ListCoursesComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   exports: [ListCoursesComponent],
 })
 export class CoursesModule {}
--- src/app/courses/list-courses/list-courses.component.ts
+++ src/app/courses/list-courses/list-courses.component.ts
@@ -1,10 +1,48 @@
+import { Course } from './../course';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-list-courses',
   templateUrl: './list-courses.component.html',
-  styleUrls: ['./list-courses.component.css']
+  styleUrls: ['./list-courses.component.css'],
 })
 export class ListCoursesComponent {
+  etatForm: boolean = false;
 
+  myCourse: Course = {
+    title: 'Learn to learn',
+    price: 0,
+  };
+
+  courses: Course[] = [
+    {
+      id: '1',
+      title: 'Learn React',
+      price: 20,
+    },
+    {
+      id: '2',
+      title: 'Learn Angular 16',
+      price: 30,
+    },
+    {
+      id: '3',
+      title: 'Learn Vue',
+      price: 25,
+    },
+  ];
+
+  toggleForm() {
+    this.etatForm = !this.etatForm;
+  }
+
+  deleteCourse(myCourse: Course) {
+    // const index = this.courses.indexOf(course);
+    // this.courses.splice(index, 1);
+
+    if (!confirm(`Are you sure to delete this course ${myCourse.title} ?`)) {
+      return;
+    }
+    this.courses = this.courses.filter((course) => course.id !== myCourse.id);
+  }
 }
