4a985cd9b2738c1c22dd24627cacc510a809955b
Mohamed IDBRAHIM <idbrahimdev@gmail.com>
2023-08-28 22:59:36 +0100

    update and delete category

--- src/app/blog/components/categories/categories.component.ts
+++ src/app/blog/components/categories/categories.component.ts
@@ -8,6 +8,7 @@ import { Category } from '../../models/category';
   styleUrls: ['./categories.component.css'],
 })
 export class CategoriesComponent implements OnInit {
+  id = signal<number>(0);
   label = signal<string>('');
   categories = signal<Category[]>([]);
 
@@ -17,14 +18,57 @@ export class CategoriesComponent implements OnInit {
     this.categoryService.getAll().subscribe((res) => this.categories.set(res));
   }
 
-  add() {
+  createOrUpdate() {
     const category: Category = {
       label: this.label(),
     };
 
+    if (this.id()) {
+      this.update(category);
+    } else {
+      this.store(category);
+    }
+  }
+
+  store(category: Category) {
     this.categoryService.persist(category).subscribe((res) => {
       this.label.set('');
       this.categories.mutate((data) => data.push(res));
+    });
+  }
+
+  update(data: Category) {
+    data.id = this.id();
+
+    this.categoryService.update(data.id, data).subscribe((res) => {
+      this.categories.update((categories) =>
+        categories.map((category) =>
+          category.id === this.id() ? data : category
+        )
+      );
+      this.label.set('');
+      this.id.set(0);
+    });
+  }
+
+  edit(category: Category) {
+    let { id, label } = category;
+
+    if (id) {
+      this.id.set(id);
+    }
+    this.label.set(label);
+  }
+
+  delete(id: number) {
+    if (!confirm('Are you to delete this item ?')) {
+      return;
+    }
+
+    this.categoryService.delete(id).subscribe((res) => {
+      this.categories.update((categories) =>
+        categories.filter((category) => category.id !== id)
+      );
     });
   }
 }
